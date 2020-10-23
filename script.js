d37777dcfe734066ab07b1e86334104c24776033
maddieritter <maddieritter46@gmail.com>
2020-10-23 18:57:59 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const testWrapper = document.querySelector(".test-wrapper");
+const testArea = document.querySelector("#test-area");
+const originText = document.querySelector("#origin-text p").innerHTML;
+const resetButton = document.querySelector("#reset");
+const theTimer = document.querySelector(".timer");
+
+var timer = [0,0,0,0];
+var interval;
+var timerRunning = false;
